8f313ce7a4209ad9928f007496a7b00b5e6073e9
x1aoba <xiaobayuwei@gmail.com>
2013-07-11 21:01:06 +0800

    login & logout

--- controllers/user.js
+++ controllers/user.js
@@ -5,21 +5,33 @@ var db =require('../config').db,
 
 exports.register = function(req, res){
   db.collection('users').save(new User(req.param('name')), function(err, user){
+    console.log(user);
     res.json(user);
   });
 };
 
 exports.login = function(req, res){
-  db.collection('users').find({ name: req.param('name')}).toArray(function(err, user){
-    if (!user.length) {
-      res.clearCookie();
-      res.send('Login Error');
-    } else {
-      db.collection('users').findAndModify({_id: user[0]._id}, [['_id', 'asc']],{ $set: { status: USER_STATUS_TABLE.ONLINE }}, { new: true }, function(err, user){
-        res.cookie('name', user.name, { signed: true });
-        res.json(user);
-      });
-    }
+  req.param('name') ? 
+    (
+      db.collection('users').find({ name: req.param('name')}).toArray(function(err, user){
+        if (!user.length) {
+          res.clearCookie();
+          res.send('Login Error');
+        } else {
+          db.collection('users').findAndModify({_id: user[0]._id}, [['_id', 'asc']],{ $set: { status: USER_STATUS_TABLE.ONLINE }}, {}, function(err, user){
+            res.cookie('name', user.name, { signed: true });
+            res.json(user);
+          });
+        }
+      })
+    ) : res.send(400);
+};
+
+exports.logout = function(req, res){
+  db.collection('users').update({_id: req.param('id') }, { $set: { status: USER_STATUS_TABLE.OFFLINE }}, function(err, result){
+    if(err){console.log(err);}
+    console.log(result);
+    res.send('bye');
   });
 };
 
